fa18cad20e84e7e571e301bd873ae2b72c28a584
Mike O'Neil <oneil@cims.nyu.edu>
2025-02-18 16:29:55 -0500

    minor edits for documentation

--- c/include/helmrouts2d.h
+++ c/include/helmrouts2d.h
@@ -1,19 +1,15 @@
 
 #include <complex.h>
 
+// these are header files for some routines that needed to be called from C
+
+// forms a multipole expansion from charges only
 void h2dformmpc_(int *nd, double complex *zk, double *rscale,  double *source,
                  int *ns, double complex *charge, double *center,
                  int *nterms, double complex *mpole);
 
-
+// performs a multipole-to-multipole translation
 void h2dmpmp_(int *nd, double complex *zk, double *rscale1, double *center1,
               double complex *hexp1, int *nterms1, double *rscale2,
               double *center2,  double complex *hexp2,  int *nterms2);
-  
-
-void h2dmpmp_(int *nd, double _Complex *zk, double *rscale0, double *center0,
-                  double _Complex *mpole0, int *nterms0,
-                  double *rscale1, double *center1,
-                  double _Complex *mpole1, int *nterms1);
-
 
--- c/include/wideband2d.h
+++ c/include/wideband2d.h
@@ -1,15 +1,20 @@
 
 
+#include <complex.h>
+
+// performs a diagonal high-frequency MP-to-MP translation
 void h2dmpmphf_(int *nd, double _Complex *zk, double *rscale0, double *center0,
                   double _Complex *mpole0, int *nterms0,
                   double *rscale1, double *center1,
                   double _Complex *mpole1, int *nterms1);
 
+// performs a diagonal high-frequency MP-to-Local translation
 void h2dmplochf_(int *nd, double _Complex *zk, double *rscale0, double *center0,
                   double _Complex *mpole0, int *nterms0,
                   double *rscale1, double *center1,
                   double _Complex *mpole1, int *nterms1);
 
+// performs a diagonal high-frequency Local-to-Local translation
 void h2dloclochf_(int *nd, double _Complex *zk, double *rscale0, double *center0,
                   double _Complex *mpole0, int *nterms0,
                   double *rscale1, double *center1,
